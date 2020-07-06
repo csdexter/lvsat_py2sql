01dae448c02944ade8f014e051453a7b79b06151
Claudiu <claudiu@tanaselia.ro>
2020-07-06 14:17:14 +0300

    Update lvsat_py2sql.py

--- lvsat_py2sql.py
+++ lvsat_py2sql.py
@@ -7,10 +7,12 @@ f_month = ""
 filepath = 'launchlogy.txt'
 with open(filepath) as fp:
    line = fp.readline()
+   line = fp.readline()
+   line = fp.readline()                             # dirty way of starting to read the file from its third line
    cnt = 1
    while line:
-       if line[22].strip()=='' : filler="0"
-       if line[18:21] == "Jan" : f_month="-01-"
+       if line[22].strip()=='' : filler="0"         # converting single-digit day number to two-digit, by adding a leading 0
+       if line[18:21] == "Jan" : f_month="-01-"     # converting dates to timestamp format accepted by PostgreSQL 
        if line[18:21] == "Feb" : f_month="-02-"
        if line[18:21] == "Mar" : f_month="-03-"
        if line[18:21] == "Apr" : f_month="-04-"
@@ -22,7 +24,7 @@ with open(filepath) as fp:
        if line[18:21] == "Oct" : f_month="-10-"
        if line[18:21] == "Nov" : f_month="-11-"
        if line[18:21] == "Dec" : f_month="-12-"
-       if line[0:1].strip() != "" : 
+       if line[0:1].strip() != "" :                 # skipping the lines dealing only with payloads (treated differently)
            print("INSERT INTO launches VALUES ('"+line[0:10].strip()
                    +"','"+line[13:18].strip()+f_month+line[22:23].strip()+filler+line[23:27]+":"+line[27:29]
                    +"','"+line[40:55].strip()
