94e9ae3ebd6d44e462361ae96cbf40e77a7fed94
Claudiu <claudiu@tanaselia.ro>
2020-07-13 09:34:28 +0300

    Update lvsat_py2tsv.py
    adjusting variable names

--- lvsat_py2tsv.py
+++ lvsat_py2tsv.py
@@ -55,28 +55,35 @@ with open(filepath_lv) as fp:
         # collecting data from sites.txt, namely lsState variable
         for y in range(len(site_array)):
             if site_array[y][0:9].strip()==(line_lv[160:169].strip()):
-                lsState = site_array[y][40:49].strip()
-                if lsState == "J" : lsState = "JP"
+                ls_state = site_array[y][40:49].strip()
+
+                # prettifying record for Japan
+                if ls_state == "J" : ls_state = "JP"
 
         # skipping the lines dealing only with payloads (we're interested in launches, not satellites)
         # and processing launchlog.txt
         if line_lv[0:1].strip() != "" :
             lv_launchID = line_lv[0:10].strip()
             lv_launchDate = line_lv[13:18].strip()+f_month+line_lv[22:23].strip()+filler+line_lv[23:27]+":"+line_lv[27:29]
+
+            # dealing with incomplete date records
             if line_lv[27:29].strip() == "" : lv_launchDate = line_lv[13:18].strip()+f_month+line_lv[22:24].strip()+filler+line_lv[23:27]
             if line_lv[25:27].strip() == "" : lv_launchDate = line_lv[13:18].strip()+f_month+line_lv[22:24].strip()+filler
+
             lv_COSPAR = line_lv[40:55].strip()
-            lv_postpayload = line_lv[55:86].strip()
-            lv_prepayload = line_lv[86:112].strip()
+            lv_postPayload = line_lv[55:86].strip()
+            lv_prePayload = line_lv[86:112].strip()
             lv_SATCAT = line_lv[112:121].strip()
             lv_type = line_lv[121:144].strip()
+
             # adjusting the names of some rockets
             if lv_type == "Soyuz-2-1A": lv_type = "Soyuz-2.1a"
             if lv_type == "Soyuz-2-1B": lv_type = "Soyuz-2.1b"
             if lv_type == "Soyuz-2-1V": lv_type = "Soyuz-2.1v"
             if lv_type == "Falcon 9"  : lv_type = "Falcon-9"
+
             lv_serial = line_lv[144:160].strip()
-            lsName = line_lv[160:169].strip()
+            ls_name = line_lv[160:169].strip()
             lv_launchPad = line_lv[169:193].strip()
             lv_outcome = line_lv[193:194].strip()
 
@@ -86,6 +93,7 @@ with open(filepath_lv) as fp:
                 # SATCAT have an extra 0 digit in satcat.txt, dealing with that
                 if sat_array[x][0:8].strip()==("S0"+line_lv[113:120].strip()):
                     match = x
+
             sat_owner = sat_array[match][89:102].strip()
             sat_orbitClass = sat_array[match][156:165].strip()
 
@@ -94,7 +102,7 @@ with open(filepath_lv) as fp:
 
             # exporting collected data
             with open(output, 'a') as f:
-                f.write(lv_launchID.ljust(12) + lv_launchDate.ljust(18) + lv_type.ljust(23) + lv_serial.ljust(20) + lv_prepayload.ljust(30) + lv_postpayload.ljust(30) + sat_owner.ljust(14) + sat_orbitClass.ljust(10) + lsState.ljust(5) + lsName.ljust(9) + lv_launchPad.ljust(25) + lv_outcome.ljust(2) + "\n")
+                f.write(lv_launchID.ljust(12) + lv_launchDate.ljust(18) + lv_type.ljust(23) + lv_serial.ljust(20) + lv_prePayload.ljust(30) + lv_postPayload.ljust(30) + sat_owner.ljust(14) + sat_orbitClass.ljust(10) + ls_state.ljust(5) + ls_name.ljust(9) + lv_launchPad.ljust(25) + lv_outcome.ljust(2) + "\n")
 
         # skipping lines with payload data only
         else: pass
